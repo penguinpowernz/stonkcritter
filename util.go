8c2efd0b15603fd67d2a92113b142a03ba8a5cff
Robert McLeod <robert@penguinpower.co.nz>
2021-12-27 01:20:31 +1300

    add a message deduplicator so users don't get sent the same message

--- util.go
+++ util.go
@@ -1,6 +1,8 @@
 package politstonk
 
 import (
+	"encoding/base64"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -16,4 +18,28 @@ func normalizeDate(ts string) time.Time {
 	}
 
 	return t
+}
+
+// deduper is a quick and dirty deduplicator, returns two closures, one to test
+// if the given pair is locked, and one to mark the given pair as locked
+func deduper() (func(int64, string) bool, func(int64, string)) {
+	locks := []string{}
+
+	shouldsend := func(chatID int64, msg string) bool {
+		msghash := base64.RawStdEncoding.EncodeToString([]byte(msg))
+		lock := strconv.Itoa(int(chatID)) + "_" + msghash
+		for _, l := range locks {
+			if l == lock {
+				return false
+			}
+		}
+		return true
+	}
+
+	marksent := func(chatID int64, msg string) {
+		msghash := base64.RawStdEncoding.EncodeToString([]byte(msg))
+		locks = append(locks, strconv.Itoa(int(chatID))+"_"+msghash)
+	}
+
+	return shouldsend, marksent
 }
