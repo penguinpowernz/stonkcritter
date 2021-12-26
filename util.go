7c76010760727237d5962d2e36f4f8a07d6c6b33
Robert McLeod <robert@penguinpower.co.nz>
2021-12-27 01:21:59 +1300

    add a helper method to escape MarkdownV2 Telegram messages

--- util.go
+++ util.go
@@ -10,6 +10,17 @@ import (
 func isTicker(s string) bool {
 	return strings.HasPrefix(s, "$")
 }
+
+func tgEscape(s string) string {
+	s = strings.ReplaceAll(s, ".", `\.`)
+	s = strings.ReplaceAll(s, "(", `\(`)
+	s = strings.ReplaceAll(s, ")", `\)`)
+	s = strings.ReplaceAll(s, "-", `\-`)
+	s = strings.ReplaceAll(s, "$", `\$`)
+	s = strings.ReplaceAll(s, ":", `\:`)
+	return s
+}
+
 func normalizeDate(ts string) time.Time {
 	t, _ := time.Parse("2006-01-02", ts)
 
