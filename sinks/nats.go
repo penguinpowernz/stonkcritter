e6501306e065f2557b63cd434079dc6ae66a86b6
Robert McLeod <robert@penguinpower.co.nz>
2022-01-20 11:12:58 +1300

    add an extra NATS sink to push the entire disclosure object as JSON

--- sinks/nats.go
+++ sinks/nats.go
@@ -7,8 +7,16 @@ import (
 
 type Sink func(models.Disclosure) error
 
+// NATSMessage will send the formatted disclosure message over NATS
 func NATSMessage(nc nats.Conn, subj string) Sink {
 	return func(d models.Disclosure) error {
 		return nc.Publish(subj, []byte(d.String()))
+	}
+}
+
+// NATS will send the full disclosure object as JSON over NATS
+func NATS(nc nats.Conn, subj string) Sink {
+	return func(d models.Disclosure) error {
+		return nc.Publish(subj, d.Bytes())
 	}
 }
